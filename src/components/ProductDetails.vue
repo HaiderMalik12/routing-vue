d3d019e4944f48fd915fbff149ed7e568054ae40
Haider Malik <haidermalik504@gmail.com>
2018-09-28 17:52:55 +0500

    finish dynamic route setup

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Product Details</h1>
+        <div class="card">
+            <div class="card-body">
+                <p>Product Id </p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'ProductDetails'
+    }
+</script>
